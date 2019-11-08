77c056034b3985ec46e93b04bf22892b134dc6c6
JennyHuang-Qaeco <55267819+JennyHuang-Qaeco@users.noreply.github.com>
2019-11-08 16:15:28 +1100

    add file and create histogram

--- Analysis.R
+++ Analysis.R
@@ -2,14 +2,21 @@
 
 
 #read data
+octopus <- data.frame(oct_id = as.factor(1:500),
+                      weight = rnorm(500,5,1))
+write.csv(octopus, 'data/octopus_data.csv', row.names = FALSE)
 
-
+octopus <- read.csv('data/octopus_data.csv')
 
 
 #process data
-
+octopus$log_weight < - log(octopus$weight)
 
 #graphing
+png('figs/hist_apple_weight.png') #this opens up a png file open to editing. 
+#this also switches the plotting device from the R default to the png
+hist(apple$weight)
+dev.off() #this line closes off the temporary png plotting device
 
 
 
